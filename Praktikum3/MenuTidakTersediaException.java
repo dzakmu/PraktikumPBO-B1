1d6445d49752a899899fdb79dad7ce492d3a26a7
dzakmu <112741896+dzakmu@users.noreply.github.com>
2024-03-08 19:09:37 +0700

    Update MenuTidakTersediaException.java

--- Praktikum3/MenuTidakTersediaException.java
+++ Praktikum3/MenuTidakTersediaException.java
@@ -1,3 +1,5 @@
+/* Muhammad Dzaky Mu'ammar */
+/* 24060122130064 */
 public class MenuTidakTersediaException extends Exception {
     public MenuTidakTersediaException(String message) {
         super(message);
